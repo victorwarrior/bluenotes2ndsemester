0d1c2a1b0643a9b83d622384788eb3df232856c6
victorwarrior <victor.halgaard.kristensen@gmail.com>
2023-03-01 21:46:09 +0100

    world map 1.1

--- witch_proto_2d/Assets/Scripts/Camera.cs
+++ witch_proto_2d/Assets/Scripts/Camera.cs
@@ -13,7 +13,7 @@ public class Camera : MonoBehaviour
 
     void Update() {
         // following the player  
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -100);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -500);
 
         // @TODO: notice camera z = -500 and background z = 500, this could cause trouble when
         //        y gets < -500 or > 500... could you move the background and the camera too?
