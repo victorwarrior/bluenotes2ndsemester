4892f704967d7f9beea12e6e4d5212e168ab896c
Oxygenexhaler <bennerjensen@gmail.com>
2023-03-05 15:45:02 +0100

    panel

--- witch_proto_2d/Assets/Scripts/Inventory/Item.cs
+++ witch_proto_2d/Assets/Scripts/Inventory/Item.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
-public class Items : MonoBehaviour
+
+public class Item 
 {
-    public int    id;
-    public string itemName;
-    public Sprite icon;
+    public enum ItemType
+    {
+        Item1,
+    }
+
+    public ItemType itemType;
+    public int amount;
 }
---
+++ witch_proto_2d/Assets/Scripts/Inventory/ToBePutInPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToBePutInPlayer : MonoBehaviour
+{
+    private Inventory inventory;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        inventory = new Inventory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
