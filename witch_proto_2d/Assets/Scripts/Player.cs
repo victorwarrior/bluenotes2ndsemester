ceade1bc9f158de5c82a5c66f2fa7aa03bb832c6
victorwarrior <victor.halgaard.kristensen@gmail.com>
2023-03-12 19:30:52 +0100

    sprint added

--- witch_proto_2d/Assets/Scripts/Player.cs
+++ witch_proto_2d/Assets/Scripts/Player.cs
@@ -7,15 +7,28 @@ public class Player : MonoBehaviour {
     // references
     Rigidbody2D rb;
 
-    // constants
-    float speed    = 40.5f; // how fast you accelerate
-    float maxSpeed = 100f;  // top speed
+    // constants               ** explanations: **
+
+    // CAREFUL WHEN MAKING CONSTANTS PUBLIC, IF PUBLIC THEY FOLLOW THE VALUE IN THE EDITOR
+    float speed       = 40.5f; // how fast you accelerate
+    float maxSpeed    = 100f;  // top speed
+    float sprintBoost = 1.6f;  // speed * sprintBoost = speed when sprinting, top speed * sprintBoost = top speed when sprinting
+    
+    float friction    = 5.85f; // used to make the character slow down, so it eventually stands still no keys are being pressed
+    
+    int stamina       = 1000;  // used for sprinting
+    int staminaMax    = 1000;  
+    int staminaDrain  = 10;    // how much stamina is used per tick when sprinting
+    int staminaRegen  = 1;     // how much stamina is gained when not sprinting
+
+    // other
+    bool staminaRegenBool = false;
 
 
     void Start() {
 
-        rb = GetComponent<Rigidbody2D>();
-        rb.drag = 5.25f; // used to make the character slow down, so it eventually stands still no keys are being pressed
+        rb      = GetComponent<Rigidbody2D>();
+        rb.drag = friction; 
 
     }
 
@@ -23,15 +36,17 @@ public class Player : MonoBehaviour {
     void FixedUpdate() {
 
         // checks and stores what keys are pressed
-        bool keyUp    = Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow);
-        bool keyDown  = Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow);
-        bool keyLeft  = Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow);
-        bool keyRight = Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow);
+        bool keyUp        = Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow);
+        bool keyDown      = Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow);
+        bool keyLeft      = Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow);
+        bool keyRight     = Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow);
 
-        // determines direction based on what keys are pressed
+        bool keySprint    = Input.GetKey(KeyCode.LeftShift);
+        bool keySprintEnd = Input.GetKeyUp(KeyCode.LeftShift);
+
+        // determines direction based on what keys are pressed @TODO: decide what pressing left then right should result in and then write something that shouldnt be touched again
         float horDir = 0f;
         float verDir = 0f;
-
         if (keyUp)   verDir += 1f;
         if (keyDown) verDir += -1f;
         if (keyLeft) {
@@ -52,9 +67,22 @@ public class Player : MonoBehaviour {
         }
 
         // moves the character in the determined direction
-        rb.AddForce(new Vector2(horDir * speed, verDir * speed));
-        if (rb.velocity.x > maxSpeed)  rb.velocity = new Vector2(maxSpeed, rb.velocity.y); // @TODO: does this enforce the wanted amount of restriction on diagonal movement too? hmm, double check
-        if (rb.velocity.y > maxSpeed)  rb.velocity = new Vector2(rb.velocity.x, maxSpeed);
+
+        float possibleSprintBoost = 1f;
+        if (keySprintEnd) staminaRegenBool = true;
+
+        if (keySprint && stamina >= staminaDrain && (keyDown || keyUp || keyRight || keyLeft)) {
+            stamina            -= staminaDrain;
+            possibleSprintBoost = sprintBoost;
+            staminaRegenBool    = false;
+        } else if (stamina <= staminaMax && staminaRegenBool) {
+            stamina += staminaRegen;
+            if (stamina > staminaMax) stamina = staminaMax;
+        }
+
+        rb.AddForce(new Vector2(horDir * speed * possibleSprintBoost, verDir * speed * possibleSprintBoost));
+        if (rb.velocity.x > (maxSpeed * possibleSprintBoost)) rb.velocity = new Vector2((maxSpeed * possibleSprintBoost), rb.velocity.y); // @TODO: does this enforce the wanted amount of restriction on diagonal movement too? hmm, double check
+        if (rb.velocity.y > (maxSpeed * possibleSprintBoost)) rb.velocity = new Vector2(rb.velocity.x, (maxSpeed * possibleSprintBoost));
 
         // update z coordinate to be in front / behind other objects
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
