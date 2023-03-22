52ec8c05229cbf2fa7af7576e90005f167c87263
Oxygenexhaler <bennerjensen@gmail.com>
2023-03-22 17:43:26 +0100

    pushobject0.3

--- witch_proto_2d/Assets/Scripts/PushObstacle.cs
+++ witch_proto_2d/Assets/Scripts/PushObstacle.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PushObstacle : MonoBehaviour
 {
     public float pushSpeed;
     public GameObject Player;
 
-    // Start is called before the first frame update
-    void Start()
+    //position and scale variables
+   
+
+    void OnCollisionEnter2D(Collision2D col)
     {
         
+        ContactPoint2D contact = col.GetContact(0);
+        switch (contact)
+        {
+            
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
