f5ed04a93d6e15eeb93c8dfb10df644a6b300ba8
Oxygenexhaler <bennerjensen@gmail.com>
2023-04-14 11:55:01 +0200

    nuck0.6

--- witch_proto_2d/Assets/Scripts/Nuckelavee.cs
+++ witch_proto_2d/Assets/Scripts/Nuckelavee.cs
@@ -5,38 +5,51 @@ using UnityEngine;
 public class Nuckelavee : MonoBehaviour
 {
     //references
-    GameObject player;
-    GameObject enemy;
-    Rigidbody2D rb;
+   public GameObject player;
+   public GameObject enemy;
+          Rigidbody2D rb;
 
     //vectors
     private Vector2 dashPosition;
+    private Vector2 enemySpawnPosition;
 
     private float spawnTime;
-    private float timeSinceSpawn;
     private float enemyCallTime;
+    private float enemyCallCD;
+    private float enemyCallCDAdd;
+    private int   enemySpawnNumber;
+
     private float dashTime;
+    private float dashCD;
+    private float dashCDAdd;
     private float dashSpeed;
+
     private float distanceToPlayer;
     private float followSpeed;
 
     public float dashDistance;
     public float dashPositionScale;
+    public float spawnRangeMaxX;
+    public float spawnRangeMaxY;
+    public float spawnRangeMinX;
+    public float spawnRangeMinY;
 
     private bool dashMode;
     // Start is called before the first frame update
     void Start()
     {
-        spawnTime = Time.time;
+        spawnTime      = Time.time;
+        
+        enemyCallTime  = spawnTime;
     }
 
     
     void FixedUpdate()
     {
+        dashTime = Time.time - spawnTime;
         distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-
         //Moves the Nuckelavee or initiates dash sequence
-        if (timeSinceSpawn >= dashTime && distanceToPlayer < dashDistance)
+        if (dashTime >= dashCD && distanceToPlayer < dashDistance)
         {
             dashMode = true;
             dashPosition = player.transform.position*dashPositionScale;
@@ -50,16 +63,28 @@ public class Nuckelavee : MonoBehaviour
         if (dashMode)
         {
             Vector2.MoveTowards(transform.position, dashPosition, dashSpeed);
+            if(transform.position.x == dashPosition.x && transform.position.y == dashPosition.y)
+            {
+                dashMode = false;
+                dashCD = dashTime + dashCDAdd;
+            }
         }
     }
 
     void Update()
     {
-        timeSinceSpawn = Time.time - spawnTime;
+       
+       enemyCallTime = Time.time - spawnTime;
 
-        if(timeSinceSpawn >= enemyCallTime)
+        if(enemyCallTime >= enemyCallCD)
         {
-
+            for(int i = 0; i < enemySpawnNumber; i++)
+            {
+                enemySpawnPosition = new Vector2(Random.Range(spawnRangeMinX, spawnRangeMaxX), Random.Range(spawnRangeMinY, spawnRangeMaxY));
+                Instantiate(enemy, enemySpawnPosition, Quaternion.identity);
+            }
+            enemyCallCD = enemyCallTime + enemyCallCDAdd;
         }
     }
+
 }
