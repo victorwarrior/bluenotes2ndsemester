26e14ddeb9bb5451f8f7aa5b5a3b5ec0c03933ea
victorwarrior <victor.halgaard.kristensen@gmail.com>
2023-04-21 10:33:28 +0200

    fixed enemy aggro (isAttacking bool)

--- witch_proto_2d/Assets/aggroRange.cs
+++ witch_proto_2d/Assets/aggroRange.cs
@@ -3,50 +3,23 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class aggroRange : MonoBehaviour
-{
-    //Constancs
-    float aggro    = 11.5f;
-    float deaggro  = 15.5f;
-    Vector2 origin;
+public class aggroRange : MonoBehaviour {
 
-    //Variables
-    GameObject enemy;
-    public bool attacking;
+    // references
+           GameObject enemy;
     public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        origin = new Vector2(0,0);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnTriggerStay2D(Collider2D col)
-    {
+    void OnTriggerStay2D(Collider2D col) {
        
 
         if (col.gameObject.tag == "Enemy")
         {
             enemy = col.gameObject;
-            //Debug.DrawLine(this.transform.position, enemy.transform.position, Color.green, 10.0f, false);
-            RaycastHit2D hit = Physics2D.Raycast(player.transform.position, (enemy.transform.position - player.transform.position).normalized, Mathf.Infinity);
-            Debug.DrawRay(player.transform.position, (enemy.transform.position - player.transform.position).normalized, Color.green, 10.0f, false);
-
-            if (hit.collider.gameObject.tag == "Enemy")
-            {
-                Debug.Log("The ray is hitting an " + hit.collider.gameObject.tag);
-                //Debug.DrawLine(transform.position, hit.collider.gameObject.transform.position, Color.green, 100.0f, false);
-                enemy.GetComponent<Enemy>().isAttacking = true;
-                attacking = true;
-            }
-            
-
-
+            RaycastHit2D hit = Physics2D.Raycast(player.transform.position,
+                                                 (enemy.transform.position - player.transform.position).normalized,
+                                                 Mathf.Infinity);
+            if (hit.collider.gameObject.tag == "Enemy") enemy.GetComponent<Enemy>().isAttacking = true;
         }
     }
 }
