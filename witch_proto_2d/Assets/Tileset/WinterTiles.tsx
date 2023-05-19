5fdd0534df0467fd28bd3b520a29d679d0b09a44
Toke113 <114912578+Toke113@users.noreply.github.com>
2023-05-19 11:21:07 +0200

    Tiled works with unity
    Experimented with creating a map in Tiled and placing it in the unity scene. Also downloaded an extension in order to make it possible.

---
+++ witch_proto_2d/Assets/Tileset/WinterTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="WinterTiles" tilewidth="1024" tileheight="1024" tilecount="6" columns="3">
+ <image source="../tiles/TILEMAP.png" width="3072" height="2048"/>
+</tileset>
