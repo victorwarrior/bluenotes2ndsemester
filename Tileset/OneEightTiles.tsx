f7563d35392e90ad56e4c5754e8911c78cb2bed9
Toke113 <114912578+Toke113@users.noreply.github.com>
2023-05-22 17:01:15 +0200

    Zoom now works
    Zooms out when entering the level and zooms in while in the level.
    
    Removed the "Camera" script and replaced it with a script called "CameraScript" as "Camera" should be a protected class and having a script with that name meant that the "Camera" component could not be accessed. Also replaced"MainCamera" with "NewCamera" so you might need to adjust the settings on the camera to suit you.
    
    Added a new gameobject "ZoomOut" which determines the area in which you can zoom out. Also added a new script to it called "ZoomOut".
    
    Added two new coroutines to "CameraScript" that allow us to zoom in and out. These are called by public functions "ZoomIn" and "ZoomOut" which are called by the "ZoomOut" script.

---
+++ Tileset/OneEightTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="OneEightTiles" tilewidth="128" tileheight="128" tilecount="384" columns="24">
+ <image source="../tiles/TILEMAP.png" width="3072" height="2048"/>
+</tileset>
---
+++ witch_proto_2d/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class CameraScript : MonoBehaviour
+{
+    // references
+    public GameObject player;
+    public Camera cam;
+
+    public float zoom = 1;
+
+    void Start()
+    {
+        //zoom = GetComponent<Camera>().
+        cam= GetComponent<Camera>();
+        cam.orthographicSize = 16f;
+    }
+
+    void Update()
+    {
+        // following the player  
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -500);
+
+
+        // @TODO: notice camera z = -500 and background z = 500, this could cause trouble when
+        //        y gets < -500 or > 500... could you move the background and the camera too?
+
+    }
+    public void ZoomOut()
+    {
+        Debug.Log("Zooming out");
+        StartCoroutine(CinematicView());
+    }
+
+    public void ZoomIn()
+    {
+        StartCoroutine(CinematicToNormalView());
+    }
+    public IEnumerator CinematicView()
+    {
+        for (float distance = cam.orthographicSize; distance <= 32f; distance += .1f)
+        {
+            zoom = distance;
+            cam.orthographicSize = distance;
+            yield return null;
+        }
+
+    }
+    public IEnumerator CinematicToNormalView()
+    {
+        for (float distance = zoom; distance >= 16f; distance -= .1f)
+        {
+            zoom = distance;
+            cam.orthographicSize = distance;
+            yield return null;
+        }
+
+    }
+}
+
